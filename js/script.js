97089d6cd29c82f83dcabfac332a7fe439e7d0e6
Agata Huczuk <agatahuczuk@cosibella.pl>
2023-01-13 16:35:55 +0100

    else if

--- js/script.js
+++ js/script.js
@@ -8,3 +8,24 @@ printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wy
 var playerMove;
 playerMove = 'papier';
 printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+
+
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
