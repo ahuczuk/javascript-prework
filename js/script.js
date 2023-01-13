ea124b365d50414c688bad3d8840a7f734e73531
Agata Huczuk <agatahuczuk@cosibella.pl>
2023-01-13 16:45:23 +0100

    playermove

--- js/script.js
+++ js/script.js
@@ -29,3 +29,23 @@ if (randomNumber == '3') {
   computerMove = 'nieznany ruch';
 }
 printMessage('Mój ruch: ' + computerMove);
+
+var playerMove, playerInput;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '2') {
+  playerMove = 'papier';
+} else {
+  playerMove = 'nieznany ruch';
+}
+if (playerInput == '3') {
+  playerMove = 'nożyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
